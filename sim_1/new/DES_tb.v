33e5d4876f3b81f5afc447a338e5e11adcda070a
Paolo Terzolo <46501344+mew27@users.noreply.github.com>
2020-09-24 16:50:49 +0200

    Add files via upload

--- sim_1/new/DES_tb.v
+++ sim_1/new/DES_tb.v
@@ -43,19 +43,77 @@ module DES_tb();
         clk = 0;
         en_s = 0;
         reset_n_s = 0;
+       
         
-        #20 en_s = 1;
-        #20 plaintext_s = 1;
-        #20 key_s = 1;
-        #20 reset_n_s = 1;
-        
-        #100 plaintext_s = 0;
-        #100 key_s = 0;
-        
-        #500 plaintext_s = 'h8787878787878787;
+        #(CLOCK_PERIOD*3) plaintext_s = 'h8787878787878787;
         key_s = 'h0e329232ea6d0d73;
-        #10 plaintext_s = 'h02468aceeca86420;
+        //cyphertext should be : 0
+        reset_n_s = 1;
+        en_s = 1;
+        #CLOCK_PERIOD plaintext_s = 'h02468aceeca86420;
         key_s = 'hf1571c947d9e859;
+        //cyphertext should be : da02ce3a89ecac3b
+        #CLOCK_PERIOD plaintext_s = 'h4369616f757a0000;
+        key_s = 'h6162656466323134;
+        //cyphertext should be : ea90d4e460e10781
+        #CLOCK_PERIOD plaintext_s = 'hAAAAAAAAAAAAAAAA;
+        key_s = 'hAAAAAAAAAAAAAAAA;
+        //cyphertext should be: C4322BE19E9A5A17
+        #CLOCK_PERIOD plaintext_s = 'h5555555555555555;
+        key_s = 'h5555555555555555;
+        //cyphertext should be: 3BCDD41E6165A5E8
+        #CLOCK_PERIOD plaintext_s = 0;
+        key_s = 0;
+        //cyphertext should be 8CA64DE9C1B123A7
+        #CLOCK_PERIOD plaintext_s = 'hFFFFFFFFFFFFFFFF;
+        key_s = 'hFFFFFFFFFFFFFFFF;
+        //cyphertext should be 7359B2163E4EDC58
+        #CLOCK_PERIOD en_s = 0; //pause the computation
+        
+        #(CLOCK_PERIOD*20) en_s = 1;
+        
+        #(CLOCK_PERIOD*11)
+        if ( cyphertext_s == 'h0000000000000000 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");
+
+        #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'hda02ce3a89ecac3b )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");            
+        
+        #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'hea90d4e460e10781 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");
+            
+        #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'hC4322BE19E9A5A17 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!"); 
+        
+        #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'h3BCDD41E6165A5E8 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");
+           
+        #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'h8CA64DE9C1B123A7 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");
+            
+         #(CLOCK_PERIOD)
+        if ( cyphertext_s == 'h7359B2163E4EDC58 )
+            $display ("Test result: negative");
+        else 
+            $display ("Test result: POSITIVE!!");           
+                      
        // key_s = 'h3031323334353637;
         //key_s = 'h133457799bbcdff1;
     end
